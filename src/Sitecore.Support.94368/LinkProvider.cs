ea5a9db96bbb16f4777c2fe43c44df21dad59801
DK\ili <ili@sitecore.net>
2017-05-24 15:07:19 +0300

    no message

--- src/Sitecore.Support.94368/LinkProvider.cs
+++ src/Sitecore.Support.94368/LinkProvider.cs
@@ -12,69 +12,30 @@ namespace Sitecore.Support.Links
 {
     public class LinkProvider : Sitecore.Links.LinkProvider
     {
-        private string ResolveDisplayNames(Item item, LinkBuilder builder, string currResult = "")
+        private string EncodeNamesRecursive(Item item, string currResult = "")
         {
             string dispName = item.DisplayName;
-            if (!String.IsNullOrEmpty(dispName))
+            
+            dispName = Sitecore.MainUtil.EncodeName(dispName);
+
+            if (String.IsNullOrEmpty(currResult))
             {
-                XmlNode repNode = Configuration.Factory.GetConfigNode("/sitecore/encodeNameReplacements");
-                if (repNode != null)
-                {
-                    foreach (XmlNode childNode in repNode.ChildNodes)
-                    {
-                        string mode = childNode != null ? childNode.Attributes["mode"].Value : String.Empty;
-                        if (String.IsNullOrEmpty(mode))
-                        {
-                            mode = "on";
-                        }
-                        if (childNode != null && mode == "on")
-                        {
-                            if (String.IsNullOrEmpty(childNode.Attributes["find"].Value))
-                            {
-                                Log.Error("'encodeNameReplacements' Error. The value of the 'Find' attribute can't be empty", this);
-                            }
-                            else if (dispName.Contains(childNode.Attributes["find"].Value))
-                            {
-                                if (String.IsNullOrEmpty(childNode.Attributes["replaceWith"].Value))
-                                {
-                                    Log.Error("'encodeNameReplacements' Error. The value of the 'replaceWith' attribute can't be empty", this);
-                                }
-                                else
-                                {
-                                    dispName = dispName.Replace(childNode.Attributes["find"].Value, childNode.Attributes["replaceWith"].Value);
-                                }
-                            }
-                        }
-                    }
-                    if (String.IsNullOrEmpty(currResult))
-                    {
-                        currResult = dispName;
-                    }
-                    else
-                    {
-                        currResult = dispName + "/" + currResult;
-                    }
-                }
+                currResult = dispName;
             }
             else
             {
-                if (String.IsNullOrEmpty(currResult))
-                {
-                    currResult = builder.GetItemUrl(item);
-                }
-                else
-                {
-                    currResult = builder.GetItemUrl(item) + "/" + currResult;
-                }
+                currResult = dispName + "/" + currResult;
             }
+
             if (item.Parent != null && item.ParentID != Sitecore.ItemIDs.ContentRoot && item.ParentID != Sitecore.ItemIDs.RootID)
             {
-               return ResolveDisplayNames(item.Parent,builder, currResult);
+                return EncodeNamesRecursive(item.Parent, currResult);
             }
             else
             {
                 return currResult;
             }
+
         }
         public override string GetItemUrl(Item item, UrlOptions options)
         {
@@ -84,7 +45,7 @@ namespace Sitecore.Support.Links
             string text = linkBuilder.GetItemUrl(item);
             if (UseDisplayName)
             {
-                text = ResolveDisplayNames(item,linkBuilder);
+                text = EncodeNamesRecursive(item);
             }
             
             if (options.LowercaseUrls)
